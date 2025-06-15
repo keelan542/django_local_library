e54e5f228e663f16bd4f1ae7fcf867003c3d8abc
keelan542 <keelanb542@gmail.com>
2025-06-15 13:35:25 +0100

    Add BookInstance Model

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
+import uuid
 
 class Genre(models.Model):
     """Model representing a book genre"""
@@ -52,3 +53,36 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for a book"""
         return reverse('book-detail', args=[str(self.id)])
+    
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book"""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this book across the whole library")
+    
+    books = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+
+    imprint = models.CharField(max_length=200)
+
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        """String for representng the Model object"""
+        return f'{self.id} ({self.book.title})'
