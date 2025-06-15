dcc57225ef8d7fbf3810f88da9aa84302193ea28
keelan542 <keelanb542@gmail.com>
2025-06-15 13:41:19 +0100

    Added Author model and migrated

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -86,3 +86,24 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representng the Model object"""
         return f'{self.id} ({self.book.title})'
+    
+class Author(models.Model):
+    """Model representing an author"""
+    first_name = models.CharField(max_length=100)
+
+    last_name = models.CharField(max_length=100)
+
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the url to access an author instance"""
+        return reverse('author-detail', args=[str(self.id)])
+    
+    def __str__(self):
+        """String for representing the Model object"""
+        return f'{self.last_name}, {self.first_name}'
