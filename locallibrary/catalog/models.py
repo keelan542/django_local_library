d22df6edcdc776ceeb10c331606bb75c17059d1e
keelan542 <keelanb542@gmail.com>
2025-06-15 13:25:42 +0100

    Add Book model

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -20,9 +20,35 @@ class Genre(models.Model):
         return reverse('genre-detail', args=[str(self.id)])
     
     class Meta:
-        constrains = [
+        constraints = [
             UniqueConstraint(
                 Lower('name'),
+                name='genre_name_case_insensitive_unique',
                 violation_error_message="Genre already exists (case insensitive match)"
-            )
+            ),
         ]
+
+class Book(models.Model):
+    """Model representing a book (not a specific copy of a book)"""
+    title = models.CharField(max_length=200)
+
+    author = models.ForeignKey('Author', on_delete=models.RESTRICT, null=True)
+
+    summary = models.TextField(
+        max_length=1000,
+        help_text="Enter a brief description of the book"
+    )
+
+    isbn = models.CharField('ISBN', max_length=13,
+                            unique=True,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+
+    def __str__(self):
+        """String for representing the Modle object"""
+        return self.title
+    
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for a book"""
+        return reverse('book-detail', args=[str(self.id)])
